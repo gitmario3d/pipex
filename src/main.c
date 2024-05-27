5bef5f0a5fcc987cb187c67b61fdcc18c0082072
Mario Alena Burgos <malena-b@c2r2s5.42malaga.com>
2024-05-27 13:53:34 +0200

    .

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: malena-b <mario3d93@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 11:42:59 by malena-b          #+#    #+#             */
-/*   Updated: 2024/05/24 12:59:21 by malena-b         ###   ########.fr       */
+/*   Updated: 2024/05/27 13:18:35 by malena-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	execute(char *cmd, char **env)
 		ft_putstr_fd("pipex: command not found: ", 2);
 		ft_putendl_fd(cmd_mat[0], 2);
 		ft_free_tab(cmd_mat);
-		exit(0);
+		exit(1);
 	}
 }
 
@@ -32,7 +32,7 @@ void	c_process(char **argv, int *pipe_fd, char **env)
 {
 	int		fd;
 
-	fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC);
+	fd = open(argv[1], O_RDONLY);
 	dup2(fd, 0);
 	dup2(pipe_fd[1], 1);
 	close(pipe_fd[0]);
@@ -42,8 +42,8 @@ void	c_process(char **argv, int *pipe_fd, char **env)
 void	p_process(char **argv, int *pipe_fd, char **env)
 {
 	int		fd;
-
-	fd = open(argv[4], O_WRONLY);
+//añadir comprobación de errores en las siguientes llamadas
+	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC , 0644);
 	dup2(fd, 1);
 	dup2(pipe_fd[0], 0);
 	close(pipe_fd[1]);
@@ -64,5 +64,9 @@ int	main(int argc, char **argv, char **env)
 		report_and_exit(3);
 	if (pid == 0)
 		c_process(argv, pipe_fd, env);
-	p_process(argv, pipe_fd, env);
+	else
+		p_process(argv, pipe_fd, env);
+	char *c=malloc(500);
+	(void)c;
+	return (0);
 }
